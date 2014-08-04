d41e5e84db8ade7dc76d5e02588e9b0787f5bc1d
Xiaoyu Ge <fantastdd@gmail.com>
2014-08-04 19:08:13 +1000

    Include new library

--- src/newstory/GenerateGraph.java
+++ src/newstory/GenerateGraph.java
@@ -1,4 +1,8 @@
 package newstory;
+import org.paukov.combinatorics.Factory;
+import org.paukov.combinatorics.Generator;
+import org.paukov.combinatorics.ICombinatoricsVector;
+
 import tool.GQRGraphGenerator;
 import tool.Log;
 import essential.PCGraph;
@@ -6,20 +10,79 @@ import essential.PCGraph;
 public class GenerateGraph {
 //List<PCGraph> gOGraphs = new LinkedList<PCGraph>();//output graphs
 StringBuilder sb = new StringBuilder();
+private int k;
 public void generateCompleteGraph(int k)
 {
+	this.k = k;
 	int[][] edges = new int[k][k];
 		int numOfEdges = k * (k - 1)/2;
 	sb.ensureCapacity((int) Math.pow(50, numOfEdges));
 	assign(0, 1, 0, edges);
+	if (sb.length() != 0)
+	{
+		Log.echo(" Generated "  + gCounter + " graphs ");
+		GQRGraphGenerator.writeGraphGQR(sb, "comp" + k);
+	}
 	//GQRGraphGenerator.parseGraphGQR(gOGraphs, "comp3");
 }
-public void generateComplete3Graph()
+public void generateRealComplete3Graph_Atomic()
 {
 	int[][] edges = new int[3][3];
 	int numOfEdges = 3;
 	sb.ensureCapacity((int) Math.pow(50, numOfEdges));
-	assign(0, 1, 0, edges);
+	
+	ICombinatoricsVector<Integer> initialVector = Factory.createVector(new Integer[]{1, 2, 4, 8, 16, 32, 64, 128});
+	// Create a multi-combination generator to generate 3-combinations of
+	   // the initial vector
+	   Generator<Integer> gen = Factory.createMultiCombinationGenerator(initialVector, 3);
+	   
+	   Log.echo(" Generated "  + gen.generateAllObjects().size() + " combinations ");
+	   for (ICombinatoricsVector<Integer> combination : gen) 
+	   {
+		   
+	   }
+}
+public void generateComplete3Graph()
+{
+	
+	int[][] edges = new int[3][3];
+	int numOfEdges = 3;
+	sb.ensureCapacity((int) Math.pow(50, numOfEdges));
+
+	Integer[] allRels = new Integer[254];
+	for (int i = 0; i < allRels.length; i++)
+	{
+		allRels[i] = i + 1;
+	}
+	// Create the initial vector of (apple, orange)
+	   ICombinatoricsVector<Integer> initialVector = Factory.createVector(
+	      allRels );
+
+	   // Create a multi-combination generator to generate 3-combinations of
+	   // the initial vector
+	   Generator<Integer> gen = Factory.createMultiCombinationGenerator(initialVector, 3);
+	  
+	   Log.echo(" Generated "  + gen.generateAllObjects().size() + " graphs ");
+	   for (ICombinatoricsVector<Integer> combination : gen) {
+	    
+	      edges[0][1] = combination.getValue(0);    
+	      edges[0][2] = combination.getValue(1);
+	      edges[1][2] = combination.getValue(2);
+	      PCGraph graph = new PCGraph(edges);
+		  GQRGraphGenerator.parseGraphGQR(graph, sb);
+		  gCounter ++;
+		  if (gCounter % 1000000 == 0)
+		  {
+				Log.echo(" Generated "  + gCounter + " graphs ");
+				GQRGraphGenerator.writeGraphGQR(sb, "comp3");
+				sb = new StringBuilder();
+			}
+	   }
+	   if (sb.length() != 0)
+	   {
+		   Log.echo(" Generated the remaining graphs ");
+		   GQRGraphGenerator.writeGraphGQR(sb, "comp3");
+	   }
 }
 int gCounter = 0;
 public void assign(int i, int j, int label, int[][] edges )
@@ -47,7 +110,7 @@ public void assign(int i, int j, int label, int[][] edges )
 				if (gCounter % 1000000 == 0)
 				{
 					Log.echo(" Generated "  + gCounter + " graphs ");
-					GQRGraphGenerator.writeGraphGQR(sb, "comp3");
+					GQRGraphGenerator.writeGraphGQR(sb, "comp" + k);
 					sb = new StringBuilder();
 				}
 			}
@@ -66,8 +129,9 @@ public static void main(String args[])
 		file.delete();
 	GQRGraphParser.generateGraphGQR(graph, "TFG_" + filename);*/
 	GenerateGraph gg = new GenerateGraph();
-	gg.generateCompleteGraph(4);
-	Log.echo("Number of graphs  " + gg.gCounter);
+	//gg.generateCompleteGraph(4);
+	gg.generateComplete3Graph();
+	//Log.echo("Number of graphs  " + gg.gCounter);
 }
 
 }
